48db38975c35eecf0ce9dabbf4e8c7c65a61bf89
Mikhail Meiko <thebestrubl@gmail.com>
2026-05-27 23:44:40 +0300

    Readme

--- src/main/java/app/finplan/config/WebConfig.java
+++ src/main/java/app/finplan/config/WebConfig.java
@@ -8,12 +8,4 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOrigins("http://localhost", "http://127.0.0.1")
-//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-//                .allowedHeaders("Content-Type", "Authorization")
-//                .allowCredentials(true);
-//    }
 }
